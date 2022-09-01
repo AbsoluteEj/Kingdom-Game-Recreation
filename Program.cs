be132ed2707bcd88bddf30bf583557713829cfb5
AbsoluteEj <ejsadiarin@gmail.com>
2022-09-01 17:27:06 +0800

    Added UI (also suggested for some functionalities)

--- Program.cs
+++ Program.cs
@@ -9,7 +9,21 @@ namespace Kingdom_Game_Recreation
             // UI
             Console.WriteLine("* *** WELCOME TO KINGDOM GAME! *** *\n");
             Console.WriteLine("Please read the instructions below for the game mechanics.");
-            Console.WriteLine("");
+            Console.WriteLine("** Winning Conditions:");
+            Console.WriteLine("  - Population is greater than 2000 ");
+            Console.WriteLine("  - Money is greater than 6000 and Population is greater than 1000");
+            Console.WriteLine("** [NOTE] If happiness or money drops to 0 or less than 0, it is GAME OVER!");
+            Console.WriteLine("Watch out for day-specific events along your journey!\n");
+            // can add functionality like if statements to start game
+            Console.WriteLine("Enter any key to Start your Kingdom Game! Let's see what you got.");
+
+            // GAME Choices UI - can be encapsulated in a method for getting input
+            Console.WriteLine("Enter ");
+            Console.WriteLine("A. Host a Banquet");
+            Console.WriteLine("B. Create Jobs");
+            Console.WriteLine("C. Hold a Festival");
+            Console.WriteLine("D. Collect Emergency Taxes");
+            Console.WriteLine("E. End Current Day");
 
             // Get user input (can create method)
             string input = Console.ReadLine();
